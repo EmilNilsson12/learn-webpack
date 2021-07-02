0dd307b5868d5009ae944a95aeabdaf549ce42e3
Emil Nilsson <52598265+EmilNilsson12@users.noreply.github.com>
2021-07-02 13:49:20 +0200

    Asset Management: Loading Data - .toml .yaml .json5

--- src/index.js
+++ src/index.js
@@ -3,9 +3,23 @@ import myName from './myName';
 import './style.css';
 import Icon from './icon.png';
 import { functionOne, functionTwo } from './myModule';
+
 import DataXML from './data.xml';
 import DataCSV1 from './data1.csv';
 import DataCSV2 from './data2.csv';
+
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
+
+console.log('toml.title: ', toml.title);
+console.log('toml.owner.name: ', toml.owner.name);
+
+console.log('yaml.title: ', yaml.title);
+console.log('yaml.owner.name: ', yaml.owner.name);
+
+console.log('json.title: ', json.title);
+console.log('json.owner.name: ', json.owner.name);
 
 function component(string = 'Jeff') {
 	const element = document.createElement('div');
